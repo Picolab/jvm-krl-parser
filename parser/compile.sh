92dcc3f49934d2e144547ca146569dfcd222a932
Matthew Wright <matthew@smallhelm.com>
2016-06-01 16:39:44 -0600

    krl_parser.jar has a main class that takes a file in and outputs the json ast

--- parser/compile.sh
+++ parser/compile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export CLASSPATH=`pwd`/lib/antlr-3.3-complete.jar:`pwd`/out/classes:.
+rm -rf out
 java org.antlr.Tool RuleSet.g -o out/src
 mkdir -p out/classes
 javac -d out/classes out/src/*.java src/org/json/*.java src/*.java
 cd out/classes
 jar -xf ../../lib/antlr-3.3-complete.jar
-jar -cf ../krl_parser.jar *
+jar -cfe ../../krl_parser.jar KRLParser *
 cd ../..
+rm -r out
